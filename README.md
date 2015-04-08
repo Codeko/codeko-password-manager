ccd9662f49ec3fb3bcc2aac83f008541311965d6
Drey91 <pjoc91@hotmail.com>
2015-04-08 10:50:03 +0200

    "#5503 Subida diseño registro final"

---
+++ README.md
@@ -0,0 +1,44 @@
+# codeko-password-manager
+A online password manager tool
+
+- Installation.
+<ol>
+<li>
+Download or clone the project from our repository.
+</li>
+<li>
+Installing Symfony.
+
+    Open a command console and execute: 
+
+        
+        $ sudo curl -LsS http://symfony.com/installer -o /usr/local/bin/symfony
+       
+        $ sudo chmod a+x /usr/local/bin/symfony       
+</li>
+<li>
+Opening the project.
+    
+    Run in a terminal 
+
+
+        composer install
+
+     or 
+
+
+        composer update
+
+    , third party bundles will be downloaded and installed into the project.
+
+    Generate project's database typing in a terminal:
+
+
+        $ php app/console doctrine:database:create
+
+    Generate project's web folder typing in a terminal:
+
+
+        $ php app/console assets:install web --symlink
+</li>
+
---
+++ app/AppKernel.php
@@ -0,0 +1,56 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel {
+
+    public function registerBundles() {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // SONATA
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            // Dependency (check that you don't already have this line)
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            // Vendor specifics bundles
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            // Then add SonataAdminBundle
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            // You have 2 options to initialize the SonataUserBundle in your AppKernel,
+            // you can select which bundle SonataUserBundle extends
+            // Most of the cases, you'll want to extend FOSUserBundle though ;)
+            // extend the ``FOSUserBundle``
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            new Codeko\LoginPrincipalBundle\CodekoLoginPrincipalBundle(),
+            // JMS
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+
+}
