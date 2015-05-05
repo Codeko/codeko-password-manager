5ace19194a44591f78ff0be641a99818c23370fb
Drey91 <pjoc91@hotmail.com>
2015-05-05 12:27:38 +0200

    Añadido voters para controlar la edición de contraseñas

--- src/Application/Sonata/UserBundle/Controller/PasswordController.php
+++ src/Application/Sonata/UserBundle/Controller/PasswordController.php
@@ -5,8 +5,9 @@ namespace Application\Sonata\UserBundle\Controller;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
 //use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Doctrine\ORM\EntityManager; 
+use Doctrine\ORM\EntityManager;
 
 /**
  * Password controller.
@@ -14,54 +15,158 @@ use Doctrine\ORM\EntityManager;
  */
 class PasswordController extends Controller {
 
-////    public function listAction()
-////    {
-////        // get a Post instance
-////        $post = null;
-////
-////        // keep in mind, this will call all registered security voters
-////        if (false === $this->get('security.authorization_checker')->isGranted('view', $post)) {
-////            throw new AccessDeniedException('Unauthorised access!');
-////        }
-////
-//////        return new Response('<h1>'.$post->getName().'</h1>');
-////        return new Response('<h1>prueba</h1>');
-////    }
-//        public function listAction()
-//    {
-//
-////            $prj = $this->getDoctrine()->getRepository('AppBundle:Project')->findOneById($id);
-////     if (false === $this->get('security.authorization_checker')->isGranted('responsible', $prj)) {
-////        throw new AccessDeniedException('Unauthorised access!');
-////    }
-//        
-//        
-////        {% if is_granted('view') %}<a href="#">Ver</a>{% endif %}
-//        
-//        
-////        if (false === $this->get('security.authorization_checker')->isGranted('view', $post)) {
-////            throw new AccessDeniedException('Unauthorised access!');
-////        }
-//            
-//        if (false === $this->admin->isGranted('LIST')) {
-//            throw new AccessDeniedException();
+    public function editAction($id = null, Request $request = null) {
+        $request = $this->resolveRequest($request);
+
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        $id = $request->get($this->admin->getIdParameter());
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+        }
+
+        if (false === $this->get('security.context')->isGranted('ROLE_EDITAR_PASSWORD', $object)) {
+            //Controlar Voters
+        }
+
+        if (false === $this->admin->isGranted('EDIT', $object)) {
+            throw new AccessDeniedException();
+        }
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->getRestMethod($request) == 'POST') {
+            $form->submit($request);
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode($request) || $this->isPreviewApproved($request))) {
+
+                try {
+                    $object = $this->admin->update($object);
+
+                    if ($this->isXmlHttpRequest($request)) {
+                        return $this->renderJson(array(
+                                    'result' => 'ok',
+                                    'objectId' => $this->admin->getNormalizedIdentifier($object)
+                                        ), 200, array(), $request);
+                    }
+
+                    $this->addFlash(
+                            'sonata_flash_success', $this->admin->trans(
+                                    'flash_edit_success', array('%name%' => $this->escapeHtml($this->admin->toString($object))), 'SonataAdminBundle'
+                            )
+                    );
+
+                    // redirect to edit mode
+                    return $this->redirectTo($object, $request);
+                } catch (ModelManagerException $e) {
+                    $this->handleModelManagerException($e);
+
+                    $isFormValid = false;
+                }
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest($request)) {
+                    $this->addFlash(
+                            'sonata_flash_error', $this->admin->trans(
+                                    'flash_edit_error', array('%name%' => $this->escapeHtml($this->admin->toString($object))), 'SonataAdminBundle'
+                            )
+                    );
+                }
+            } elseif ($this->isPreviewRequested($request)) {
+                // enable the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+                $this->admin->getShow();
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+                    'action' => 'edit',
+                    'form' => $view,
+                    'object' => $object,
+                        ), null, $request);
+    }
+
+    public function listAction(Request $request = null) {
+        $request = $this->resolveRequest($request);
+        
+        if (false === $this->admin->isGranted('LIST')) {
+            throw new AccessDeniedException();
+        }
+        
+        if ($listMode = $request->get('_list_mode', 'mosaic')) {
+            $this->admin->setListMode($listMode);
+        }
+        
+        $datagrid = $this->admin->getDatagrid();
+        $filters = $request->get('filter');
+
+//        if (false === $this->get('security.context')->isGranted('ROLE_MOSTRAR_PASSWORD', $datagrid)) {
+//            //Controlar Voters
 //        }
-//        
-////        if (false === $this->get('security.authorization_checker')->isGranted('view')) {
-////            throw new AccessDeniedException('Unauthorised access!');
-////        }
-//
-//        $datagrid = $this->admin->getDatagrid();
-//        $formView = $datagrid->getForm()->createView();
-//
-//        // set the theme for the current Admin Form
-//        $this->get('twig')->getExtension('form')->renderer->setTheme($formView, $this->admin->getFilterTheme());
-//
-//        return $this->render($this->admin->getTemplate('list'), array(
-//            'action'     => 'list',
-//            'form'       => $formView,
-//            'datagrid'   => $datagrid,
-//            'csrf_token' => $this->getCsrfToken('sonata.batch'),
-//        ));
-//    }
+        
+        if (!$filters || !array_key_exists('context', $filters)) {
+            $context = $this->admin->getPersistentParameter('context', $this->get('sonata.media.pool')->getDefaultContext());
+        } else {
+            $context = $filters['context']['value'];
+        }
+        $datagrid->setValue('context', null, $context);
+
+        $category = $this->container->get('sonata.classification.manager.category')->getRootCategory($context);
+        if (!$filters) {
+            $datagrid->setValue('category', null, $category->getId());
+        }
+        if ($request->get('category')) {
+            $contextInCategory = $this->container->get('sonata.classification.manager.category')->findBy(array(
+                'id' => (int) $request->get('category'),
+                'context' => $context
+            ));
+            if (!empty($contextInCategory)) {
+                $datagrid->setValue('category', null, $request->get('category'));
+            } else {
+                $datagrid->setValue('category', null, $category->getId());
+            }
+            
+        }
+        $formView = $datagrid->getForm()->createView();
+
+        $this->get('twig')->getExtension('form')->renderer->setTheme($formView, $this->admin->getFilterTheme());
+        return $this->render($this->admin->getTemplate('list'), array(
+                    'action' => 'list',
+                    'form' => $formView,
+                    'datagrid' => $datagrid,
+                    'root_category' => $category,
+                    'csrf_token' => $this->getCsrfToken('sonata.batch'),
+        ));
+    }
+
+    // FUNCION PRIVADA DE SONATAADMIN-CRUDCONTROLLER, HAY QUE LLAMARLA DESDE AQUI
+    /**
+     * To keep backwards compatibility with older Sonata Admin code.
+     *
+     * @internal
+     */
+    private function resolveRequest(Request $request = null) {
+        if (null === $request) {
+            return $this->getRequest();
+        }
+        return $request;
+    }
+
 }
--- src/Application/Sonata/UserBundle/Security/Authorization/Voter/PasswordVoter.php
+++ src/Application/Sonata/UserBundle/Security/Authorization/Voter/PasswordVoter.php
@@ -4,94 +4,45 @@ namespace Application\Sonata\UserBundle\Security\Authorization\Voter;
 
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Doctrine\ORM\EntityManager; 
 
-class PasswordVoter implements VoterInterface
-{
-    const VIEW = 'view';
-    const EDIT = 'edit';
+class PasswordVoter implements VoterInterface {
 
-    public function supportsAttribute($attribute)
-    {
-        return in_array($attribute, array(
-            self::VIEW,
-            self::EDIT,
-        ));
+    public function supportsAttribute($attribute) {
+        return 'ROLE_EDITAR_PASSWORD' == $attribute;
     }
 
-    public function supportsClass($class)
-    {
-        $supportedClass = 'Application\Sonata\UserBundle\Entity\Password';
-
-        return $supportedClass === $class || is_subclass_of($class, $supportedClass);
+    public function supportsClass($class) {
+        return true;
     }
 
-    /**
-     * @var \Application\Sonata\UserBundle\Entity\Password $post
-     */
-    public function vote(TokenInterface $token, $pass, array $attributes)
-    {
-        // check if class of this object is supported by this voter
-        if (!$this->supportsClass(get_class($pass))) {
-//            return VoterInterface::ACCESS_ABSTAIN;
-//            throw new \InvalidArgumentException(
-//                'No se carga la entidad $pass'
-//            );
-//            return VoterInterface::ACCESS_ABSTAIN;
-            return VoterInterface::ACCESS_GRANTED;
-        }
+    public function vote(TokenInterface $token, $object, array $attributes) {
+        
+        $vote = VoterInterface::ACCESS_ABSTAIN;
+        
+        foreach ($attributes as $attribute) {
+            if (false === $this->supportsAttribute($attribute)) {
+                continue;
+            }
+            $user = $token->getUser();  
+            $idpassword = $object->getId();
+            
+            $vote = VoterInterface::ACCESS_DENIED;
 
-        // check if the voter is used correct, only allow one attribute
-        // this isn't a requirement, it's just one easy way for you to
-        // design your voter
-        if (1 !== count($attributes)) {
-            throw new \InvalidArgumentException(
-                'Only one attribute is allowed for VIEW or EDIT'
-            );
-        }
+            if (!$user->isSuperAdmin()){
 
-        // set the attribute to check against
-        $attribute = $attributes[0];
-
-        // check if the given attribute is covered by this voter
-        if (!$this->supportsAttribute($attribute)) {
-            return VoterInterface::ACCESS_ABSTAIN;
-//            return VoterInterface::ACCESS_GRANTED;
-        }
-
-        // get current logged in user
-        $user = $token->getUser();
-
-        // make sure there is a user object (i.e. that the user is logged in)
-        if (!$user instanceof UserInterface) {
-            return VoterInterface::ACCESS_DENIED;
-//            return VoterInterface::ACCESS_GRANTED;
-        }
-
-        switch($attribute) {
-            case self::VIEW:
-                // the data object could have for example a method isPrivate()
-                // which checks the Boolean attribute $private
-                if (!$pass->isPrivate()) {
-                    return VoterInterface::ACCESS_GRANTED;
-//                    return VoterInterface::ACCESS_DENIED;
+                // comprobar que la PASSWORD que se edita fue publicada por mismo usuario
+                /* @var $idpassword type */
+                if ($idpassword != $user->getId()) {
+                    $vote = VoterInterface::ACCESS_DENIED;
+                    throw new \InvalidArgumentException(
+                            'No eres el propietario'
+                        );
                 }
-                break;
-
-            case self::EDIT:
-                // we assume that our data object has a method getOwner() to
-                // get the current owner user entity for this data object
-                if ($user->getId() === $pass->getOwner()->getId()) {
-                    return VoterInterface::ACCESS_GRANTED;
-//                    return VoterInterface::ACCESS_DENIED;
-                }
-                break;
-        }
-
-        return VoterInterface::ACCESS_DENIED;
-//        return VoterInterface::ACCESS_GRANTED;
+                
+            }
+        } 
+        
+        return $vote;
     }
+
 }
-
-
