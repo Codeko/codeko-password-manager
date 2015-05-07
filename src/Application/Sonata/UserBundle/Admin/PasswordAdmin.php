a4784bef82dca81933e0531efc8f08ae93b69d39
Drey91 <pjoc91@hotmail.com>
2015-05-07 08:54:09 +0200

    Retoque controlador y css

--- src/Application/Sonata/UserBundle/Admin/PasswordAdmin.php
+++ src/Application/Sonata/UserBundle/Admin/PasswordAdmin.php
@@ -27,17 +27,17 @@ class PasswordAdmin extends Admin {
     protected function configureListFields(ListMapper $listMapper) {
         $listMapper
                 ->addIdentifier('titulo')
-                ->add('user')
                 ->add('usernamePass')
-                ->add('url', 'url')
                 ->add('password')
+                ->add('url', 'url')
                 ->add('comentario')
+                ->add('tipoPassword')
                 ->add('fechaExpira')
                 ->add('fechaCreacion')
                 ->add('fechaModificacion')
                 ->add('fechaUltimoAcceso')
 //                ->add('category')
-                ->add('tipoPassword')
+                ->add('user')
 
         ;
     }
