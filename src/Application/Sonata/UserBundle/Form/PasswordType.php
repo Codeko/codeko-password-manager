981b2dbe2dbc052085c821a37d2a830e130b832e
Drey91 <pjoc91@hotmail.com>
2015-05-11 09:32:43 +0200

    Arreglos varios

---
+++ src/Application/Sonata/UserBundle/Form/PasswordType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PasswordType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('titulo')
+                ->add('user', null, array(
+                    'class' => 'ApplicationSonataUserBundle:User',
+                    'property' => 'user',
+                    'allow_add' => true,
+                    'allow_delete' => true))
+                ->add('nombreUsuario')
+                ->add('url')
+                ->add('password')
+                ->add('comentario')
+                ->add('fechaExpira', null, array('class' => 'sonata-medium-date-custom'))
+                ->add('fechaCreacion')
+                ->add('fechaModificacion')
+                ->add('fechaUltimoAcceso')
+                ->add('tipoPassword')
+                ->add('category', null, array(
+                    'class' => 'ApplicationSonataClassificationBundle:Category',
+                    'property' => 'name',
+                    'allow_add' => true,
+                    'allow_delete' => true
+        ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Application\Sonata\UserBundle\Entity\Password'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'application_sonata_userbundle_password';
+    }
+
+}
---
+++ src/Application/Sonata/UserBundle/Security/Authorization/Voter/PasswordVoter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Security\Authorization\Voter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class PasswordVoter implements VoterInterface {
+
+    const VERPASSWORD = 'ROLE_LISTAR_ENTIDAD';
+    const EDITARPASSWORD = 'ROLE_EDITAR_ENTIDAD';
+    const BORRARPASSWORD = 'ROLE_BORRAR_ENTIDAD';
+
+    public function supportsAttribute($attribute) {
+        return in_array($attribute, array(
+            self::VERPASSWORD,
+            self::EDITARPASSWORD,
+            self::BORRARPASSWORD,
+        ));
+    }
+
+    public function supportsClass($class) {
+        return true;
+    }
+
+    public function vote(TokenInterface $token, $object, array $attributes) {
+
+        $vote = VoterInterface::ACCESS_ABSTAIN;
+
+        foreach ($attributes as $attribute) {
+            if (false === $this->supportsAttribute($attribute)) {
+                continue;
+            }
+
+            $vote = VoterInterface::ACCESS_DENIED;
+
+            if ($attribute === 'ROLE_EDITAR_ENTIDAD' || $attribute === 'ROLE_BORRAR_ENTIDAD') {
+                $user = $token->getUser();
+                $iduser = $user->getId();
+                $idpasswordPropietario = $object->getUser()->getId();
+
+                if (!$user->isSuperAdmin()) {
+
+                    /* @var $idpasswordPropietario type */
+                    if ($idpasswordPropietario != $iduser) {
+                        $vote = VoterInterface::ACCESS_DENIED;
+                        return $vote;
+                    }
+                    $vote = VoterInterface::ACCESS_GRANTED;
+                    return $vote;
+                } else {
+                    $vote = VoterInterface::ACCESS_GRANTED;
+                    return $vote;
+                }
+            }
+
+            if ($attribute === 'ROLE_LISTAR_ENTIDAD') {
+//                $user = $token->getUser();
+//                $iduser = $user->getId();
+//                $idpasswordPropietario = $object->getUser()->getId();
+//
+//                if (!$user->isSuperAdmin()) {
+//
+//                    if ($idpasswordPropietario != $iduser) {
+//                        $vote = VoterInterface::ACCESS_DENIED;
+//                        return $vote;
+//                    }
+//                    $vote = VoterInterface::ACCESS_GRANTED;
+//                    return $vote;
+//                } else {
+//                    $vote = VoterInterface::ACCESS_GRANTED;
+//                    return $vote;
+//                }
+            }
+        }
+
+        return $vote;
+    }
+
+}
