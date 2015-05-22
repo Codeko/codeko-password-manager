8063bd1341316be85bc108a03d484280b6ec6047
Drey91 <pjoc91@hotmail.com>
2015-05-22 09:25:23 +0200

    refs #6058 Añadido evento de escucha de caducidad de claves

--- src/Application/Sonata/EventListener/ExpiredPassword.php
+++ src/Application/Sonata/EventListener/ExpiredPassword.php
@@ -7,10 +7,7 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Application\Sonata\UserBundle\Entity\Password;
-use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * @Service("request.set_messages_count_listener")
@@ -33,20 +30,28 @@ class ExpiredPassword {
     public function onCheckExpired(GetResponseEvent $event) {
 
         if (($this->security_context->getToken() ) && ( $this->security_context->isGranted('IS_AUTHENTICATED_FULLY') )) {
-            $idUsuarioActivo = $this->security_context->getToken()->getUser()->getId();
-            $sql = "SELECT titulo, fechaExpira FROM Password WHERE user_id = '" . $idUsuarioActivo . "'";
-            $connection = $this->em->getConnection();
-            $statement = $connection->prepare($sql);
-            $statement->execute();
-            $results = $statement->fetchAll();
 
-            foreach ($results as $valor) {
-                $fecha = $valor["fechaExpira"];
-                $segundos = strtotime($fecha) - strtotime('now');
-                $diferencia_dias = intval($segundos / 60 / 60 / 24);
-                if (!empty($valor["fechaExpira"])) {
-                    if ($diferencia_dias <= 0) {
-                        $this->session->getFlashBag()->add('sonata_flash_error', 'Su clave  "' . $valor["titulo"] . '" ha caducado');
+            $route_name = $event->getRequest()->get('_route');
+
+            if ($route_name === "sonata_admin_dashboard") {
+
+                $idUsuarioActivo = $this->security_context->getToken()->getUser()->getId();
+                $sql = "SELECT titulo, fechaExpira FROM Password WHERE user_id = '" . $idUsuarioActivo . "'";
+                $connection = $this->em->getConnection();
+                $statement = $connection->prepare($sql);
+                $statement->execute();
+                $results = $statement->fetchAll();
+
+                foreach ($results as $valor) {
+                    $fecha = $valor["fechaExpira"];
+                    $segundos = strtotime($fecha) - strtotime('now');
+                    $diferencia_dias = intval($segundos / 60 / 60 / 24);
+                    
+                    if (!empty($valor["fechaExpira"])) {
+                        if ($diferencia_dias <= 0) {
+                            $response = new RedirectResponse($this->router->generate('fos_user_change_password'));
+                            $this->session->getFlashBag()->add('sonata_flash_error', 'Su clave "' . $valor["titulo"] . '" ha caducado');
+                        }
                     }
                 }
             }
